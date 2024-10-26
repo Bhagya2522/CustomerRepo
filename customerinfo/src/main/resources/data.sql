96bceb0ff3cb2d5a876b1fe6bb9efb832ebc5243
Bhagya2522 <bhagyashreevm25@gmail.com>
2024-10-26 21:10:12 +0530

    deleted redundant folder

--- customerinfo/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customer (first_name, last_name, email) VALUES ('Adhrit', 'Mane', 'adhrit.mane@gmail.com');
-INSERT INTO customer (first_name, last_name, email) VALUES ('Bhagya', 'Jadhav', 'bhagya.jadhav@gmail.com');
